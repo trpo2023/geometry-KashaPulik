108b5f4f4924e428b3d9446d4f6652063742b194
Kulik Pavel <KashaPulik@users.noreply.github.com>
2023-02-12 23:49:50 +0700

    Make fuction inputError

--- circle.c
+++ circle.c
@@ -1,15 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 
 #define RED_COLOR "\x1b[31m"
 #define DEFOLT_COLOR "\x1b[0m"
 
-typedef struct point{
-	int x;
-	int y;
-}point;
+typedef struct point {
+    int x;
+    int y;
+} point;
 
-typedef struct circle{
-	struct point centre;
-	int radius;
-}circle;
+typedef struct circle {
+    struct point centre;
+    int radius;
+} circle;
 
+_Bool inputError(int argc)
+{
+    if (argc < 2) {
+        printf(RED_COLOR "Error: " DEFOLT_COLOR "no files to read\n");
+        return 1;
+    }
+    if (argc > 2) {
+        printf(RED_COLOR "Error: " DEFOLT_COLOR "too many arguments\n");
+        return 1;
+    }
+    return 0;
+}
