ba99855cb314928135681e5ab9b0e08d54f821b3
Kulik Pavel <KashaPulik@users.noreply.github.com>
2023-02-20 23:50:38 +0700

    Add function lineError

--- circle.c
+++ circle.c
@@ -157,6 +157,38 @@ void lineError(int line)
     exit(1);
 }
 
+void bracketError(char *str, int line, int column, int key)
+{
+    char brackets[3] = "()";
+    _Bool flag = 0;
+    int i = 0;
+    column++;
+
+    while(1) {
+        if(str[i] == '\n')
+            break;
+
+        if((str[i] == EOF) || (str[i] == '\0')) {
+            flag = 1;
+            break;
+        }
+        i++;
+    }
+
+    printf("%s", str);
+
+    if(flag)
+        printf("\n");
+
+    for(i = 1; i < column; i++)
+        printf(" ");
+
+    printf("^\n");
+    
+    printf(RED_COLOR "Error" DEFOLT_COLOR " in line %d, column %d: expected '%c'\n", line, column, brackets[key]);
+    exit(1);
+}
+
 int main(int argc, char* argv[])
 {
     if (inputError(argc))
