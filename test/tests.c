04c3a37d4e8bf71eccc8921e654e6e45b967f909
Kulik Pavel <KashaPulik@users.noreply.github.com>
2023-04-14 12:54:19 +0700

    Add simple test

---
+++ test/tests.c
@@ -0,0 +1,16 @@
+#include "../thirdparty/ctest.h"
+
+CTEST(arithmetic_suite, simple_sum)
+{
+    // Given
+    const int a = 1;
+    const int b = 2;
+
+    // When
+    const int result = a + b;
+
+    // Then
+    const int expected = 3;
+    ASSERT_EQUAL(expected, result);
+}
+
